a7fb4a6edc0ebb6f876fa38e4c034812ad2bb6cc
Oscar <oscar-me@outlook.com>
2020-04-07 23:15:59 -0500

    edit 2

--- ZombieArena/main.cpp
+++ ZombieArena/main.cpp
@@ -7,3 +7,20 @@
 //
 
 #include <stdio.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+int main() {
+    sf::VideoMode vm(500, 500);
+    sf::RenderWindow window(vm, "ZombieArena");
+    window.setFramerateLimit(30);
+    
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+}
